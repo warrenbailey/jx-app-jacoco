d5c928c9d51674e5abc5c2b0b659357e713d7894
Pete Muir <pmuir@bleepbleep.org.uk>
2018-10-09 16:50:21 +0100

    feat: configure the team namespace

--- jacoco_analyzer_upgrade.sh
+++ jacoco_analyzer_upgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 helm repo update
-helm upgrade ${JX_JACOCO_ANALYZER_UPGRADE_CHART_REPOSITORY}/ext-jacoco --version ${EXT_VERSION}
+helm upgrade ${JX_JACOCO_ANALYZER_UPGRADE_CHART_REPOSITORY}/ext-jacoco --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE}
