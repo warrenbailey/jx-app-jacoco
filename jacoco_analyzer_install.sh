6b61066a72be4e6cb32e088ae31ea564999a5714
Pete Muir <pmuir@bleepbleep.org.uk>
2018-10-10 09:55:31 +0100

    fix: a nicer name

--- jacoco_analyzer_install.sh
+++ jacoco_analyzer_install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 helm repo update
-helm install ${JX_JACOCO_ANALYZER_INSTALL_CHART_REPOSITORY}/ext-jacoco --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE}
+helm install ${JX_JACOCO_ANALYZER_INSTALL_CHART_REPOSITORY}/ext-jacoco --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE} --name=ext-jacoco-analyzer
