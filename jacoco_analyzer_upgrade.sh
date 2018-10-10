eb9117c714d8aaee0c72ac633a4733eecc0105fd
Pete Muir <pmuir@bleepbleep.org.uk>
2018-10-10 14:57:24 +0100

    fix: set the release name

--- jacoco_analyzer_upgrade.sh
+++ jacoco_analyzer_upgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 helm repo update
-helm upgrade ${JX_JACOCO_ANALYZER_UPGRADE_CHART_REPOSITORY}/ext-jacoco --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE}
+helm upgrade ext-jacoco ${JX_JACOCO_ANALYZER_UPGRADE_CHART_REPOSITORY}/ext-jacoco --version ${EXT_VERSION} --set teamNamespace=${EXT_TEAM_NAMESPACE}
