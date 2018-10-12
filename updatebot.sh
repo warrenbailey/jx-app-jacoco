5a2701f7baa8d3ab7641712f7c51eee6f0e6d8b0
Pete Muir <pmuir@bleepbleep.org.uk>
2018-10-12 09:36:23 +0100

    fix: add repo file type to updatebot

--- updatebot.sh
+++ updatebot.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-updatebot push-regex -r "\s+tag: (.*)" -v $(cat VERSION) --previous-line "\s+remote: github.com/${ORG}/${APP_NAME}" jenkins-x-extension-definitions.yaml
+updatebot push-regex -r "\s+tag: (.*)" -v $(cat VERSION) --previous-line "\s+remote: github.com/${GITHUB_ORG}/${APP_NAME}" jenkins-x-extension-definitions.yaml
+updatebot push-regex -r "\s+tag: (.*)" -v $(cat VERSION) --previous-line "\s+remote: github.com/${GITHUB_ORG}/${APP_NAME}" jenkins-x-extensions-repository.yaml
