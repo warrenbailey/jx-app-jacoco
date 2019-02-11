5db7614f51b036c8715c38f9ce2d4d0f280cb379
Hardy Ferentschik <hardy@ferentschik.de>
2019-02-11 15:58:44 +0100

    fix: introducing top level done channel and ability to shutdown cleanly
    fixes issue #72

--- cmd/jacoco/main.go
+++ cmd/jacoco/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/jenkins-x-apps/jx-app-jacoco/internal/cluster"
 	"github.com/jenkins-x-apps/jx-app-jacoco/internal/util"
 	jenkinsclientv1 "github.com/jenkins-x/jx/pkg/client/clientset/versioned/typed/jenkins.io/v1"
@@ -8,6 +9,9 @@ import (
 	"k8s.io/client-go/rest"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 var logger = log.WithFields(log.Fields{"app": "jacoco"})
@@ -31,17 +35,70 @@ func main() {
 	}
 
 	ns := util.TeamNameSpace()
-	logger.Infof("Watching namespace %s", ns)
+	logger.Infof("watching namespace %s", ns)
 
-	cluster.WatchPipelineActivity(ns, jxClient)
+	var wg sync.WaitGroup
+	done := make(chan struct{})
+	defer close(done)
 
-	// no-op HTTP handler for health and liveliness checks
-	http.HandleFunc("/", handler)
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err.Error())
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		setupSignalChannel(done)
+		return
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		cluster.WatchPipelineActivity(done, ns, jxClient)
+		logger.Info("cluster activity monitor has shut down")
+		return
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		startHTTPServer(done)
+		logger.Info("HTTP server has shut down")
+		return
+	}()
+
+	wg.Wait()
+	logger.Info("jacoco has successfully shut down")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func startHTTPServer(done chan struct{}) {
+	server := &http.Server{Addr: ":8080"}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	go func() {
+		// returns ErrServerClosed on graceful close
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			logger.Errorf("ListenAndServe(): %s", err)
+			done <- struct{}{}
+		}
+	}()
+
+	select {
+	case <-done:
+		server.Shutdown(context.TODO())
+	}
+
+	return
+}
+
+// setupSignalChannel registers a listener for Unix signals for a ordered shutdown
+func setupSignalChannel(done chan struct{}) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+
+	go func() {
+		logger.Info("waiting for shutdown signal in the background")
+		<-sigChan
+		logger.Info("received SIGTERM signal - initiating shutdown")
+		done <- struct{}{}
+	}()
 }
